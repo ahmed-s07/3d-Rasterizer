32ad1e3c2ee369e19907413c8e592ea5d7b54173
ahmed_s <firespark07@gmail.com>
2025-12-30 21:59:43 -0500

    Now able to render multiple isntances of the cbe class at once.

--- code.py
+++ code.py
@@ -13,51 +13,15 @@ pygame.display.set_caption("engine")
 pixels = pygame.PixelArray(screen)
 pixels[:] = (0, 0, 0)
 
-red = (255, 0, 0)
-green = (0, 255, 0)
-blue = (0, 0 , 255) 
-yellow = (255, 255, 0)
-cyan = (0, 255, 255)
-magenta = (255, 0, 255)
 
-def vectorShift(verticies, t):
-    for v in verticies:
-        v[0] += t[0]
-        v[1] += t[1]
-        v[2] += t[2]
+
 
 T = [-1.5, 0, 7]
 
-# --- Data ---
-v0 = [1, 1, 1]
-v1 = [-1, 1, 1]
-v2 = [-1, -1, 1]
-v3 = [1, -1, 1]
-v4 = [1, 1, -1]
-v5 = [-1, 1, -1]
-v6 = [-1, -1, -1]
-v7 = [1, -1, -1]
-
-t0 = [0, 1, 2, red]
-t1 = [0, 2, 3, red]
-t2 = [4, 0, 3, green]
-t3 = [4, 3, 7, blue]
-t4 = [5, 4, 7, blue]
-t5 = [5, 7, 6, blue]
-t6 = [1, 5, 6, yellow]
-t7 = [1, 6, 2, yellow]
-t8 = [4, 5, 1, magenta]
-t9 = [4, 1, 0, magenta]
-t10 = [2, 6, 7, cyan]
-t11 = [2, 7, 3, cyan]
-
-verticies = [v0, v1, v2, v3, v4, v5, v6, v7]
-triangles = [t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11]
-
-
-vectorShift(verticies, T)
-
-r.renderObject(verticies, triangles, pixels)
+bruh = r.cube(position=T)
+bruh2 = r.cube(position=[1.5, 0, 50])
+r.render_instance(pixels, bruh)
+r.render_instance(pixels, bruh2)
 
 
 # --- Main Loop ---
--- renderer.py
+++ renderer.py
@@ -14,6 +14,12 @@ def interpolate(i0, d0, i1, d1):
         i += 1
     return values
 
+def vectorShift(verticies, t):
+    for v in verticies:
+        v[0] += t[0]
+        v[1] += t[1]
+        v[2] += t[2]
+
 # Helper to center coordinates
 def put_pixel(pixels, x, y, color):
     # Check bounds to prevent crashes
@@ -105,36 +111,46 @@ def fill_triangle(pixels, p0, p1, p2, color):
                     shaded = tuple(int(t * hseg[x - xl]) for t in color)
                     put_pixel(pixels, x, y, shaded)
 
-def render_Object(verticies, triangles, pixels):
+def render_object(pixels, verticies, triangles):
     projected = []
     for i in verticies:
         projected.append(project_vertex(i))
     for e in triangles:
         draw_wireframe_triangle(pixels, projected[e[0]], projected[e[1]], projected[e[2]], e[3])
 
-class model:
-    name = "cube"
-    v0 = [1, 1, 1]
-    v1 = [-1, 1, 1]
-    v2 = [-1, -1, 1]
-    v3 = [1, -1, 1]
-    v4 = [1, 1, -1]
-    v5 = [-1, 1, -1]
-    v6 = [-1, -1, -1]
-    v7 = [1, -1, -1]
+class cube:
+    
+    t0 = [0, 1, 2, settings.red]
+    t1 = [0, 2, 3, settings.red]
+    t2 = [4, 0, 3, settings.green]
+    t3 = [4, 3, 7, settings.blue]
+    t4 = [5, 4, 7, settings.blue]
+    t5 = [5, 7, 6, settings.blue]
+    t6 = [1, 5, 6, settings.yellow]
+    t7 = [1, 6, 2, settings.yellow]
+    t8 = [4, 5, 1, settings.magenta]
+    t9 = [4, 1, 0, settings.magenta]
+    t10 = [2, 6, 7, settings.cyan]
+    t11 = [2, 7, 3, settings.cyan]
 
-    t0 = [0, 1, 2, red]
-    t1 = [0, 2, 3, red]
-    t2 = [4, 0, 3, green]
-    t3 = [4, 3, 7, blue]
-    t4 = [5, 4, 7, blue]
-    t5 = [5, 7, 6, blue]
-    t6 = [1, 5, 6, yellow]
-    t7 = [1, 6, 2, yellow]
-    t8 = [4, 5, 1, magenta]
-    t9 = [4, 1, 0, magenta]
-    t10 = [2, 6, 7, cyan]
-    t11 = [2, 7, 3, cyan]
-
-    verticies = [v0, v1, v2, v3, v4, v5, v6, v7]
     triangles = [t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11]
+
+    def __init__(self, position):
+        self.position = position
+        self.triangles = cube.triangles
+        v0 = [1, 1, 1]
+        v1 = [-1, 1, 1]
+        v2 = [-1, -1, 1]
+        v3 = [1, -1, 1]
+        v4 = [1, 1, -1]
+        v5 = [-1, 1, -1]
+        v6 = [-1, -1, -1]
+        v7 = [1, -1, -1]
+        self.verticies = [v0, v1, v2, v3, v4, v5, v6, v7]
+        vectorShift(self.verticies, position)
+
+    
+
+def render_instance(pixels, instance):  
+    render_object(pixels, instance.verticies, instance.triangles)
+
